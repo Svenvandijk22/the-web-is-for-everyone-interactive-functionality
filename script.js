e3e4510eb9d5291b78f18aa2179224ac87abb772
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-03-30 14:38:40 +0200

    #6 script in me public gezet

--- script.js
+++
@@ -1,6 +0,0 @@
-const filterKnop = document.getElementById("filterKnop");
-const filterMenu = document.getElementById("filterMenu");
-
-filterKnop.addEventListener("click", function () {
-    filterMenu.classList.toggle("open");
-});
