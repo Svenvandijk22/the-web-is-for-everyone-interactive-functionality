a8da00553565a895c132149ed4760e054fb04779
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-03-31 11:13:01 +0200

    #5 verwijder de producten

--- server.js
+++ server.js
@@ -1,53 +1,54 @@
 // Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+import express from "express";
 
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
-import { Liquid } from 'liquidjs';
+import { Liquid } from "liquidjs";
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+const app = express();
 
 // Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine("liquid", engine.express());
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
+app.set("views", "./views");
 
-
-
-app.get('/', async function (request, response) {
-   // Render index.liquid uit de Views map
-   // Geef hier eventueel data aan mee
-     const params = {
+app.get("/", async function (request, response) {
+  // Render index.liquid uit de Views map
+  // Geef hier eventueel data aan mee
+  const params = {
     // Sorteer op naam
     // 'sort': 'name',
- 
+
     // Geef aan welke data je per persoon wil terugkrijgen
-    'fields': 'name,image,id',
- 
+    fields: "name,image,id",
+
     // Combineer meerdere filters
- 
-  }
+  };
 
- const productResponse = await fetch('https://fdnd-agency.directus.app/items/milledoni_products/?' + new URLSearchParams(params))
- 
-const productResponseJSON = await productResponse.json()
-  console.log(productResponseJSON.data)
-   response.render('index.liquid',{products: productResponseJSON.data}) 
-})
+  const productResponse = await fetch(
+    "https://fdnd-agency.directus.app/items/milledoni_products/?" +
+      new URLSearchParams(params),
+  );
 
+  const productResponseJSON = await productResponse.json();
+  console.log(productResponseJSON.data);
+  response.render("index.liquid", { products: productResponseJSON.data });
+});
 
-console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
+console.log(
+  "Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.",
+);
 
 /*
 // Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
@@ -113,41 +114,57 @@ app.get("/likedproducts", async function (req, res) {
   });
 });
 
-
 app.use(express.urlencoded({ extended: true }));
 
-
-
-app.post("/", async function (request, response) {
-
+app.post("/opslaan", async function (request, response) {
   console.log(request.body);
 
-   
-    await fetch(
-      "https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          milledoni_users_id: 63,
-          milledoni_products_id: request.body.id,
-        }),
-        headers: {
-          "Content-Type": "application/json;charset=UTF-8",
-        },
+  await fetch(
+    "https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        milledoni_users_id: 63,
+        milledoni_products_id: request.body.id,
+      }),
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
       },
-    );
-   
+    },
+  );
 
-  
+  response.redirect(303, "/likedproducts");
+});
+
+app.post("/verwijder", async function (request, response) {
+  const linkIDresponse = await fetch(`https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1?filter[milledoni_users_id][_eq]=63&filter[milledoni_products_id][_eq]=${request.body.id}&fields=id&limit=1`)
+  const linkIDjson = await linkIDresponse.json();
+  const linkID = linkIDjson.data[0].id;
+
+  // kan dit simpeler???? 
+
+  const deleteResponse = await fetch('https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1/' + linkID,
+    {
+      method: "DELETE", 
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      }
+    },
+  );
+
+  console.log(deleteResponse);
+
   response.redirect(303, "/likedproducts");
 });
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
-})
+  console.log(
+    `Daarna kun je via http://localhost:${app.get("port")}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`,
+  );
+});
